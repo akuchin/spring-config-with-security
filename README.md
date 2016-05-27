4d5c0461eb5ea84bf21fe0d102ed41e69749025b
Artem Kuchyn <aakuchin@gmail.com>
2016-05-27 18:53:33 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+to run:
+
+```mvn spring-boot:run```
+
+
+to test:
+
+
+```curl foo:pass@localhost:8080/foo/dev```
+
+```curl bar:pass@localhost:8080/bar/dev```
---
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/bar/**").hasAnyRole("bar")
+                .antMatchers("/foo/**").hasAnyRole("foo")
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("bar").password("pass").roles("bar");
+        auth.inMemoryAuthentication().withUser("foo").password("pass").roles("foo");
+    }
+}
